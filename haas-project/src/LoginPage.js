d9d21a93fbce1bd79631705f78cbf26ab29cc8be
Nikunj-Zamwar <89663831+Nikunj-Zamwar@users.noreply.github.com>
2023-10-31 14:56:10 -0500

    Revert "wrote code to read/update the login information from mongoDB"
    This reverts commit d0fc7de5b018a1750267b1e9d6d3517e15e6968a.

--- haas-project/src/LoginPage.js
+++ haas-project/src/LoginPage.js
@@ -1,127 +1,83 @@
-import React, { useState } from "react";
-import { Paper, Container, Grid, TextField, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import Avatar from "@mui/material/Avatar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
-import PersonIcon from "@mui/icons-material/Person";
-import Typography from "@mui/material/Typography";
-import { deepPurple } from "@mui/material/colors";
-import Axios from "axios";
+import React from "react";
+import {Paper, Container, Grid, TextField, Button} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import PersonIcon from '@mui/icons-material/Person';
+import Typography from '@mui/material/Typography';
+import { deepPurple, indigo } from '@mui/material/colors';
 
 const LoginForm = ({ onLogin }) => {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+    const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-  
-    try {
-      const response = await Axios.post("flask-backend/login.py/login", {
-        username: username,
-        password: password,
-      });
-  
-      if (response.status === 200) {
-        navigate("/projects");
-      } else {
-        setError("Invalid username or password");
-      }
-    } catch (error) {
-      setError("An error occurred during login");
-    }
-  };
-  
-  const handleRegistration = async () => {
-    try {
-      const response = await Axios.post("http://localhost:3000/register", {
-        username: username,
-        password: password,
-      });
-  
-      if (response.status === 201) {
-        navigate("/projects");
-      } else {
-        setError("User already exists");
-      }
-    } catch (error) {
-      setError("An error occurred during registration");
-    }
-  };
-  
+    const handleLogin = async () => {
+        const isAuthenticated = true;
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: deepPurple[300] }}>
-          <PersonIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          HAAS Project
-        </Typography>
-        <Box component="form" noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="userid"
-            label="User ID"
-            name="userid"
-            autoComplete="userid"
-            autoFocus
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            onClick={handleLogin}
-          >
-            Sign In
-          </Button>
-          <Button
-            fullWidth
-            variant="contained"
-            sx={{ mt: 1, mb: 2 }}
-            onClick={handleRegistration}
-          >
-            Register
-          </Button>
-          <Grid container>
-            <Grid item>
-              <p style={{ color: "red" }}>{error}</p>
-              <Link href="./createaccount" variant="body2">
-                {"Don't have an account? Create Account"}
-              </Link>
+        if (isAuthenticated) {
+        navigate('/projects');
+        }
+    };
+    
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: deepPurple[300] }}>
+            <PersonIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            HAAS Project
+          </Typography>
+          <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="userid"
+              label="User ID"
+              name="userid"
+              autoComplete="userid"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="./createaccount" variant="body2">
+                  {"Don't have an account? Create Account"}
+                </Link>
+              </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-    </Container>
-  );
-};
+      </Container>
+      );
+    
+}
 
 export default LoginForm;
